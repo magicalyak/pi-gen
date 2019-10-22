28ca373c0f5324a820c87661a9d99a57d22ae940
magicalyak <tgamull@redhat.com>
2019-10-22 07:22:59 -0400

    using just curlssh

--- stage7/00-teslausb-tweaks/00-run.sh
+++ stage7/00-teslausb-tweaks/00-run.sh
@@ -10,9 +10,9 @@ install -m 755 files/enable_wifi.sh "${ROOTFS_DIR}/root/bin"
 install -m 755 files/setup-teslausb "${ROOTFS_DIR}/root/bin"
 
 # Works for initial CURL run
-on_chroot << EOF
-ln -s /etc/ssl/certs/ca-certificates.crt /usr/local/share/ca-certificates/ca-certificates.crt
-EOF
+#on_chroot << EOF
+#ln -s /etc/ssl/certs/ca-certificates.crt /usr/local/share/ca-certificates/ca-certificates.crt
+#EOF
 
 # Below here is the rest of the stage2 (builds the Stretch lite image)
 # run script commented out just to give guidance on things that can be done.
