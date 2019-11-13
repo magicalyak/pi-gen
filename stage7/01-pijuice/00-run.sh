2d727a33f4a2892e832590fd9ad2f3e977b60f69
magicalyak <tom.gamull@gmail.com>
2019-11-13 09:35:14 -0500

    fixes

--- stage7/01-pijuice/00-run.sh
+++ stage7/01-pijuice/00-run.sh
@@ -6,5 +6,6 @@ install -v -d                                 "${ROOTFS_DIR}/var/lib/pijuice"
 install -v -m 600 files/pijuice_config.JSON   "${ROOTFS_DIR}/var/lib/pijuice/"
 on_chroot <<EOF
 systemctl enable pijuice
+systemctl disable fake-hwclock
 EOF
 fi
